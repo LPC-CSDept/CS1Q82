3370ad4317dd9c4a975eef5065af92e2f8aa2ed9
Kyuwoong Lee <kwooler@gmail.com>
2022-10-28 19:33:03 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "main.hpp"
+
+int countsubstring(string);
+int main()
+{
+	int cnt;
+
+	cnt = countsubstring("th");
+	// cnt = countsubstring("la");
+	cout << "The count of occurrence " << cnt << endl;
+}
---
+++ main.hpp
@@ -0,0 +1,12 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int countsubstring(string keystring)
+{
+	int count = 0;
+	// **************************************************
+	// **** Make your code
+	// **************************************************
+	return count;
+}
